693234ce442263bc0c10375c6d06983ac720fcfe
JoshuaGoerner <joshua.goerner@gmail.com>
2017-03-10 15:06:30 +0100

    Finished task 3.3

---
+++ models/DataFramePreprocessor.py
@@ -0,0 +1,200 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+
+class DataFramePreprocessor(object):
+    """ Preprocessor to normalize continuous and encode categorical values
+    """
+        
+    def __init__(self, encode=True, normalize=False):
+        """ Initialize new object
+        
+        INPUT:
+        ------
+        encode:     Flag to enable one-hot-encoding, default=True
+        normalize:  Flag to enable normalization, default=False
+        
+        
+        OUTPUT:
+        -------
+        New DataFramePreprocessor object
+        
+        
+        DESCRIPTION:
+        ------------
+        Based on the encode & normalization flag the DataFramePreprocessor will
+        - one-hot-encode categorical columns
+        - normalize continuous columns
+        """
+        self.normalize = normalize
+        self.encode = encode
+        self.label_encoder = None
+        self.scaler = None
+        self.cat_columns = None
+        self.con_columns = None
+        
+    
+    def fit_transform_label(self, label):
+        """ Encode and transform label
+        
+        INPUT:
+        ------
+        label:
+        
+        OUTPUT:
+        -------
+        y:
+        
+        
+        DESCRIPTION:
+        ------------
+        Based on the internal instance of sklearn.preprocessing.LabelEncoder
+        the class labels will be encoded and transformed.
+        """
+        self.label_encoder = LabelEncoder()
+        dt_label = pd.DataFrame(self.label_encoder.fit_transform(label.values.ravel()),
+                               index = label.index,
+                               columns = label.columns)
+        return dt_label
+        
+        
+    def fit_transform(self, attributes, label=None):
+        """ Learn preprocessing parameter and transform dataframe
+        
+        INPUT:
+        ------
+        attributes:  Input dataframe containing the attributes
+        label:       Optional dataframe containing class labels
+        
+        
+        OUTPUT:
+        -------
+        X:           Dataframe containing preprocessed attributes
+        y:           Optional dataframe containing encoded class labels
+        
+        
+        DESCRIPTION:
+        ------------
+        Based on the 'encode' and 'normalize' parameter, the method will
+        
+        - one-hot-encode categorical attributes
+        - normalize continuous attributes
+        - pass bool attributes without processing
+        - encode labels
+        
+        and return the dataframe(s).
+        """
+        # Split label column from attributes if given
+        if(label):
+            dt_label = attributes.loc[:,label].to_frame()
+            attributes = attributes.loc[:, attributes.columns != label]
+        else:
+            dt_label = None
+            
+        # Encode categorical columns if given
+        dt_cat = attributes.select_dtypes(include=["O"])
+        self.cat_columns = dt_cat.columns
+        if self.encode:
+            dt_cat = pd.get_dummies(dt_cat)
+            
+        # Normalize continuous colums if given
+        dt_con = attributes.select_dtypes(include=["int64", "float64"])
+        self.con_columns = dt_con.columns
+        if self.normalize:
+            self.scaler = StandardScaler()
+            dt_con = pd.DataFrame(self.scaler.fit_transform(dt_con), 
+                                       index=dt_con.index, 
+                                       columns=dt_con.columns)
+            
+        # Pass bool colums
+        dt_bool = attributes.select_dtypes(include=["bool"])
+        
+        # Encode label if given
+        if label:
+            #self.label_encoder = LabelEncoder()
+            #dt_label[label] = self.label_encoder.fit_transform(dt_label.values.ravel())
+            dt_label = self.fit_transform_label(dt_label)
+
+        
+        # Return merged dataframe + label (if given)
+        return pd.concat([dt_cat, dt_con, dt_bool], axis=1), dt_label
+    
+    
+    def inverse_transform_label(self, label):
+        """ Invert encoded labels back to original class labels
+        
+        INPUT:
+        ------
+        label:   Dataframe containing encoded class labels
+        
+        
+        OUTPUT:
+        -------
+        y:       Dataframe containing original class labels
+        
+        
+        DESCRIPTION:
+        ------------
+        Based on the internal instance of sklearn.preprocessing.LabelEncoder
+        the encoded class labels will be inverted and transformed.
+        """
+        return pd.DataFrame(self.label_encoder.inverse_transform(label),
+                            index = label.index,
+                            columns = label.columns)
+    
+    
+    def inverse_transform(self, attributes, label=None):
+        """ Invert, transform and merge dataframe(s) containing preprocessed attributes
+        
+        INPUT:
+        ------
+        attributes:  Dataframe containing the preprocessed attributes
+        labels:      Dataframe containing the encoded labels
+        
+        
+        OUTPUT:
+        -------
+        Xy:          Dataframe containing attributes (optionally labels) in original formats
+        
+        
+        DESCRIPTION:
+        ------------
+        Based on the 'encode' and 'normalize' parameter, the method will
+        
+        - restore categorical attributes which have been one-hot-encoded
+        - de-normalize continuous attributes to original interval ranges
+        - pass bool attributes without processing
+        - revert encode labels to original class labels
+        
+        and return the merged dataframe.
+        """
+        
+        # Inverse continuous columns
+        dt_con = pd.DataFrame(self.scaler.inverse_transform(attributes[self.con_columns]),
+                                                           index = attributes[self.con_columns].index,
+                                                           columns = attributes[self.con_columns].columns)
+        
+        # Inverse label dataframe
+        dt_label = None
+        if label is not None:
+            dt_label = self.inverse_transform_label(label)
+            
+        # Pass bool columns
+        dt_bool = attributes.select_dtypes(include=["bool"])
+        
+        # Inverse categorical columns - black magic following...
+        dt_cat = None
+        dt_cat_candidate = attributes[attributes.columns.difference(np.concatenate([dt_bool.columns,
+                                                                                    dt_con.columns]))]
+        for col in self.cat_columns:
+            candidates = list(filter(lambda x: x.startswith(col), dt_cat_candidate.columns))
+            values = list(map(lambda x: x.replace("{}_".format(col), ""), candidates))
+            mapping = {idx : name for idx, name in enumerate(values)}
+            aux_vector = np.arange(len(values))
+            dt_cat =  pd.concat([dt_cat,
+                                 pd.DataFrame((dt_cat_candidate.loc[:, candidates]*aux_vector).apply(np.sum, axis=1).map(mapping),
+                                              index = dt_cat_candidate.loc[:, candidates].index,
+                                              columns = [col]
+                                              )], axis=1)
+            
+        # Return label dataframe
+        return pd.concat([dt_cat, dt_con, dt_bool, dt_label], axis=1)
--- models/Utilities.py
+++ models/Utilities.py
@@ -3,6 +3,10 @@ import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.colors import BoundaryNorm, LinearSegmentedColormap
 from matplotlib.cm import jet
+from IPython.display import Image
+import pydotplus
+import itertools
+
 
 def plot_decision_regions(classifier, data, resolution=1000, legend=True, centroids=None):
     """Plotting decision regions given a classifier and data
@@ -71,4 +75,73 @@ def plot_decision_regions(classifier, data, resolution=1000, legend=True, centro
         plt.legend(loc="best")
     
     # return plot object
+    return(plt)
+
+
+def plot_decision_tree(X, y, dtree, path="tree.dot"):
+    """ Visualize Decision Tree structures
+    
+    INPUT:
+    ------
+    X:      Dataframe containing the attributes the decision tree used
+    y:      Dataframe containing the class labels the decision tree used
+    dtree:  The trained decision tree
+    path:   Path to store the .dot tree file, default='tree.dot'
+    
+    
+    OUTPUT:
+    -------
+    image:  Image of the visualized decision tree
+    
+    
+    DESCRIPTION:
+    ------------
+    The visualization of the decision tree is based on the following steps:
+    - export the tree structure as a .dot
+    - create a png based on the .dot
+    - return an image based on the png
+    
+    """
+    export_graphviz(dtree,
+                    out_file=path,
+                    feature_names=X.columns,
+                    class_names=np.unique(dfp.inverse_transform_label(y)),
+                    filled = True,
+                    rounded = True)
+    graph = pydotplus.graph_from_dot_file(path)
+    graph.write_png(path)
+    return Image(graph.create_png(), width=500)
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    
+    Method taken from:
+    http://scikit-learn.org/stable/auto_examples/
+    model_selection/plot_confusion_matrix.html#
+    sphx-glr-auto-examples-model-selection-plot-confusion-matrix-py
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
     return(plt)
